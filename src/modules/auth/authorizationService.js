1ebba6504f0fbc66f28aa2d9adad982a33768502
Yasar Bayar <yasarbayar@gmail.com>
2016-09-11 18:30:20 +0300

    ..

--- src/modules/auth/authorizationService.js
+++ src/modules/auth/authorizationService.js
@@ -3,23 +3,20 @@
 
     angular.module('aptAuth')
         .factory('aptAuthorizationService', [
-            'aptAuthEnumService', 'UserService', '$rootScope',
-            function (enums, UserService, $rootScope) {
-                var User      = UserService.getAuthUser();
-                var authorize = function (loginRequired, requiredPermissions, permissionCheckType) {
+            'aptAuthEnumService', 'UserService',
+            function (enums, UserService) {
+                var User         = UserService.getAuthUser();
+                var authorize    = function (loginRequired, requiredPermissions, permissionCheckType) {
                     var result             = enums.authorised.authorised,
-                        //user = authentication.getCurrentLoginUser(),
                         loweredPermissions = [],
                         hasPermission      = true,
                         permission, i;
 
                     permissionCheckType = permissionCheckType || enums.permissionCheckType.atLeastOne;
-                    //if (loginRequired === true && user === undefined) {
                     if (loginRequired === true && User.is_authenticated === false) {
                         result = enums.authorised.loginRequired;
-                        //} else if ((loginRequired === true && user !== undefined) &&
                     } else if ((loginRequired === true && User.is_authenticated === true) &&
-                        (requiredPermissions === undefined || requiredPermissions.length === 0)) {
+                               (requiredPermissions === undefined || requiredPermissions.length === 0)) {
                         // Login is required but no specific permissions are specified.
                         result = enums.authorised.authorised;
                     } else if (loginRequired === true && User.is_authenticated === true && User.user.login === 'superuser') {
@@ -32,12 +29,7 @@
                         });
 
                         for (i = 0; i < requiredPermissions.length; i += 1) {
-                            //if(angular.isString(requiredPermissions[i])) {
                             permission = requiredPermissions[i].toLowerCase();
-                            //} else {
-                            //    permission = requiredPermissions[i];
-                            //
-                            //}
 
                             if (permissionCheckType === enums.permissionCheckType.combinationRequired) {
                                 hasPermission = hasPermission && loweredPermissions.indexOf(permission) > -1;
@@ -59,13 +51,14 @@
 
                     return result;
                 };
+                var isAuthorized = function (loginRequired, requiredPermissions, permissionCheckType) {
+                    return authorize(loginRequired, requiredPermissions, permissionCheckType) == enums.authorised.authorised;
+                };
 
-                // $rootScope.$on('event:auth-loginRequired', function () {
-                //     User.is_authenticated = false;
-                // });
 
                 return {
-                    authorize: authorize
+                    authorize   : authorize,
+                    isAuthorized: isAuthorized
                 };
             }]);
 }(angular));
